a49338718ee58e56d77b8b5a3ce9c774ff7f817c
chanawh <thechanwh@gmail.com>
2025-02-05 19:35:47 -0800

    nav bar and pages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,28 @@
-// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My Portfolio</h1>
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <h1>Welcome to My Portfolio</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
